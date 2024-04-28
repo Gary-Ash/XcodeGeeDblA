e7302643f026dfbba6464ca5b341614e163a65c2
Gary Ash <gary.ash@icloud.com>
2024-04-28 15:04:27 -0400

    Initial commit.

---
+++ Extension/SourceEditorExtension.swift
@@ -0,0 +1,19 @@
+/*****************************************************************************************
+ * SourceEditorExtension.swift
+ * 
+ * This file contains extension boilerplate
+ *
+ * Author   :  Gary Ash <gary.ash@icloud.com>
+ * Created  :  28-Apr-2024  3:03pm
+ * Modified :
+ *
+ * Copyright © 2024 By Gary Ash All rights reserved.
+ ****************************************************************************************/
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+	func extensionDidFinishLaunching() {
+	}
+}
---
+++ XcodeGeeDblA/XcodeGeeDblAApp.swift
@@ -0,0 +1,22 @@
+/*****************************************************************************************
+ * XcodeGeeDblAApp.swift
+ *
+ * This file implements the application object
+ *
+ * Author   :  Gary Ash <gary.ash@icloud.com>
+ * Created  :  28-Apr-2024  3:03pm
+ * Modified :
+ *
+ * Copyright © 2024 By Gary Ash All rights reserved.
+ ****************************************************************************************/
+
+import SwiftUI
+
+@main
+struct XcodeGeeDblAApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
