431e095b6256a92f6c5e3726f309e1fe7747fb6f
Gary Ash <gary.ash@icloud.com>
2024-07-07 19:19:02 -0400

    Initial commit.

---
+++ XcodeGeeDblA/XcodeGeeDblAApp.swift
@@ -0,0 +1,22 @@
+/*****************************************************************************************
+ * XcodeGeeDblAApp.swift
+ *
+ * This file implements the application object
+ *
+ * Author   :  Gary Ash <gary.ash@icloud.com>
+ * Created  :   7-Jul-2024   3:36pm
+ * Modified :
+ *
+ * Copyright © 2024 By Gary Ash All rights reserved.
+ ****************************************************************************************/
+
+import SwiftUI
+
+@main
+struct XcodeGeeDblAApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
